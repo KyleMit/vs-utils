227d4fbad58e36b69bfb49ed74035d9f26645157
kylemit <Kylemit@gmail.com>
2020-09-17 19:37:42 -0400

    add command to remove duplicate lines

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,61 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vs-utils" is now active!');
+	// log activation
+	console.log('The extension "vs-utils" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vs-utils.refreshCspell', () => {
-		// The code you place here will be executed every time your command is executed
+
+	let refreshCspell = vscode.commands.registerCommand('vs-utils.refreshCspell', () => {
+
 		// https://github.com/streetsidesoftware/vscode-spell-checker/blob/v1.9.0/packages/client/package.json#L92
 		vscode.commands.executeCommand("cSpell.disableForGlobal");
 		vscode.commands.executeCommand("cSpell.enableForGlobal");
-			// Display a message box to the user
+
+		// Display a message box to the user
 		vscode.window.showInformationMessage('CSpell Refreshed');
 	});
 
-	context.subscriptions.push(disposable);
+	let removeDuplicates = vscode.commands.registerCommand('vs-utils.removeDuplicates', () => {
+
+		// Get the active text editor
+		let editor = vscode.window.activeTextEditor;
+
+		if (!editor) {
+			vscode.window.showInformationMessage(`no active text editor`);
+			return;
+		}
+
+		let document = editor.document;
+		let selection = editor.selection;
+
+		// Get the word within the selection
+		let selText = document.getText(selection);
+
+		let lines = selText.split('\n');
+		let dedupeLines = [...new Set(lines)];
+		let dedupeText = dedupeLines.join('\n');
+
+		editor.edit(editBuilder => {
+			editBuilder.replace(selection, dedupeText);
+		});
+
+		// build message
+		let lineCount = lines.length;
+		let finalCount = dedupeLines.length;
+		let duplicateCount = lineCount - finalCount;
+		let pluralize = (count:Number, noun:String) => `${count} ${noun}${(count !== 1 ? "s" : "")}`;
+
+		let msg = `Removed ${pluralize(duplicateCount, "duplicate")} of ${pluralize(lineCount, "line")}`;
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage(msg);
+	});
+
+
+	context.subscriptions.push(refreshCspell);
+	context.subscriptions.push(removeDuplicates);
 }
 
 // this method is called when your extension is deactivated
